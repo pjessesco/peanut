d7ae88ecaa604cefb27c8a0e231d585828fe8907
Jino Park <pjessesco@gmail.com>
2022-07-04 01:14:20 +0900

    [WIP] Write comments in `matrix.h`

--- Peanut/impl/matrix.h
+++ Peanut/impl/matrix.h
@@ -39,31 +39,96 @@
 
 namespace Peanut {
 
+    /**
+     * @brief Base class of every matrix expressions in Peanut.
+     * @details Peanut implements matrix expression by following CRTP concept
+     *          to achieve a static polymorphism. Every derived class should
+     *          follow its interface even they are not forced by the compiler.
+     *          Also they MUST provide their data type via `using Type = XXX`.
+     * @tparam E Derived class. `MatrixExpr`
+     */
     template <typename E>
     struct MatrixExpr{
+
+        /**
+         * @brief Get element in \p r 'th row and \p c 'th column.
+         * @details It is also used for evaluation of an arbitrary matrix expression
+         *          by recursively calling in derived classes. See also
+         *          `Matrix::Matrix(const MatrixExpr<E> &expr)` and
+         *          `Matrix::elem(Index r, Index c)` and other derived classes.
+         * @param r Row index.
+         * @param c Col index.
+         * @return Rvalue of an element in \p r 'th row and \p c 'th column.
+         */
         inline auto elem(Index r, Index c) const{
             return static_cast<const E&>(*this).elem(r, c);
         }
 
+        /**
+         * @brief Get a reference of element in \p r 'th row and \p c 'th column.
+         * @param r Row index.
+         * @param c Col index.
+         * @return Reference of an element in \p r 'th row and \p c 'th column.
+         */
         inline auto& elem(Index r, Index c) {
             return static_cast<const E&>(*this).elem(r, c);
         }
 
+        /**
+         * @brief Row size of the expression \p E.
+         */
         static constexpr Index row = E::row;
+
+        /**
+         * @brief Column size of the expression \p E.
+         */
         static constexpr Index col = E::col;
     };
 
+    /**
+     * @brief Basic matrix class.
+     * @tparam T Data type.
+     * @tparam Row Row size.
+     * @tparam Col Col size.
+     */
     template<typename T, Index Row, Index Col> requires std::is_arithmetic_v<T> && (Row > 0) && (Col > 0)
     struct Matrix : public MatrixExpr<Matrix<T, Row, Col>>{
+
+        /**
+         * @brief Data type. See the detailed description of \p MatrixExpr.
+         */
         using Type = T;
+
+        /**
+         * @brief Constructor without any parameters initialize to zero matrix.
+         */
         Matrix() {m_data.fill(t_0);}
-        
+
+        /**
+         * @brief Constructor with initializer list.
+         * @param tlist Initializer list with \p T types.
+         */
         template <typename ...TList> requires std::conjunction_v<std::is_same<T, TList>...>
         Matrix(TList ... tlist) : m_data{std::forward<T>(tlist)...} {}
-        
+
+        /**
+         * @brief Constructor with std::array.
+         * @param data STL Array having `T` type and \p Row * \p Col size.
+         */
         explicit Matrix(const std::array<T, Row * Col> &data) : m_data{data} {}
+
+        /**
+         * @brief Constructor with std::vector.
+         * @param data STL vector having `T` type.
+         */
         explicit Matrix(const std::vector<T> &data) : m_data{data} {}
 
+        /**
+         * @brief Constructor from arbitrary Peanut matrix expression.
+         * @details Lazy evaluation is performed when the given expression is
+         *          substituted to other `Matrix`, or `Matrix::eval()` is called.
+         * @param expr Arbitrary Peanut matrix expression.
+         */
         template<typename E>
         Matrix(const MatrixExpr<E> &expr) requires is_equal_type_size_v<E, Matrix>{
             for(Index r=0;r<Row;r++){
@@ -73,8 +138,16 @@ namespace Peanut {
             }
         }
 
+        /**
+         * @brief Equivalent with `Matrix::Matrix()`, but for explicit purpose.
+         * @return Zero matrix with given \p Row and \p Col .
+         */
         static Matrix zeros() {return Matrix();}
 
+        /**
+         * @brief Construct identity matrix. Available only for square matrix case.
+         * @return Identity matrix with given \p Row and \p Col .
+         */
         static Matrix identity() requires is_square_v<Matrix> {
             Matrix a;
             a.m_data.fill(t_0);
@@ -84,12 +157,23 @@ namespace Peanut {
             return a;
         }
 
-        // Static polymorphism implementation of MatrixExpr
+        /**
+         * @brief Implementation of `MatrixExpr::elem()` which returns rvalue.
+         * @param r Row index.
+         * @param c Col index.
+         * @return Rvalue of an element in \p r 'th row and \p c 'th column.
+         */
         inline T elem(Index r, Index c) const{
             assert((0<=r) && (r < Row) && (0<=c) && (c < Col));
             return m_data[Col*r+c];
         }
 
+        /**
+         * @brief Implementation of `MatrixExpr::elem()` which returns a reference.
+         * @param r Row index.
+         * @param c Col index.
+         * @return Reference of an element in \p r 'th row and \p c 'th column.
+         */
         inline T& elem(Index r, Index c) {
             assert((0<=r) && (r < Row) && (0<=c) && (c < Col));
             return m_data[Col*r+c];
