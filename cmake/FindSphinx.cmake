ffec7f568d66b9e41b3fc48a67f10afb08efd344
Jino Park <pjessesco@gmail.com>
2022-07-06 01:10:14 +0900

    Deploy documents by github action

---
+++ cmake/FindSphinx.cmake
@@ -0,0 +1,13 @@
+# Reference : https://devblogs.microsoft.com/cppblog/clear-functional-c-documentation-with-sphinx-breathe-doxygen-cmake/#catcutifier-cmake-findsphinx-cmake
+
+#Look for an executable called sphinx-build
+find_program(SPHINX_EXECUTABLE
+        NAMES sphinx-build
+        DOC "Path to sphinx-build executable")
+
+include(FindPackageHandleStandardArgs)
+
+#Handle standard arguments to find_package like REQUIRED and QUIET
+find_package_handle_standard_args(Sphinx
+        "Failed to find sphinx-build executable"
+        SPHINX_EXECUTABLE)
